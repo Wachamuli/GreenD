8a8d191d31de3f8009afdd2cdde27ef10aaf9970
wachamuli <josemrr27@gmail.com>
2024-06-07 09:53:15 -0400

    Sensitive data moved to env vars in Supabase setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,17 +3,20 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./supabase.types";
 
-const supabaseUrl = "https://zeawfjfjjegnpsfvfapw.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplYXdmamZqamVnbnBzZnZmYXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU4ODk3MzIsImV4cCI6MjAyMTQ2NTczMn0.vJEMme6ZF8gSz1bTEw1wNHGwUDSkzBujUA8-FwagUyo";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY as string;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
+const supabase = createClient<Database>(
+  supabaseUrl,
+  supabaseKey,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
   },
-});
+);
 
 export { supabase };
