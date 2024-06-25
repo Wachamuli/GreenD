ca81044a88e9c7fae60d006f66b423be89b3d19a
Wachamuli <josemrr27@gmail.com>
2024-06-25 17:33:13 -0400

    Search bar for services has been added

--- src/components/controls/SearchBar.tsx
+++ src/components/controls/SearchBar.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import {
+  FlatList,
+  Keyboard,
+  Modal,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
 import {
   horizontalScale,
   moderateScale,
@@ -7,8 +15,69 @@ import {
 import { ColorPalette } from "../../styles/colorPalette";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Txt from "../info/Txt";
+import { useCallback, useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+import Tappable from "./Tappable";
+import { router } from "expo-router";
+
+const SearchItem = ({ id, name }: { id: string; name: string }) => {
+
+  return (
+    <Tappable onPress={() => {
+      router.navigate({
+        pathname: "/home/details",
+        params: { serviceId: id },
+      });
+    }}>
+      <View
+        style={{
+          borderBottomWidth: moderateScale(0.5),
+          borderBottomColor: ColorPalette.tertiary,
+          paddingHorizontal: horizontalScale(10),
+          paddingVertical: verticalScale(15),
+        }}>
+        <Txt>{name}</Txt>
+      </View>
+    </Tappable>
+  );
+};
 
 const SearchBar = () => {
+  const [isSearching, setIsSearching] = useState(false);
+  const [services, setServices] = useState([{ id: "", name: "" }]);
+
+  const getServices = async () => {
+    const { data } = await supabase.from("services").select("id, name");
+    setServices(data);
+  };
+
+  useEffect(() => {
+    if (isSearching) getServices();
+  }, [isSearching]);
+
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setKeyboardVisible(true); // or some other action
+      },
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setKeyboardVisible(false); // or some other action
+      },
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
@@ -16,10 +85,27 @@ const SearchBar = () => {
           placeholder="Buscar"
           placeholderTextColor={"#9ca3af"}
           style={styles.textInput}
+          onFocus={() => setIsSearching(true)}
+          onBlur={() => setIsSearching(false)}
         />
         <View style={styles.iconContainer}>
           <FontAwesomeIcon icon={faMagnifyingGlass} size={20} color="#9ca3af" />
         </View>
+      </View>
+      <View>
+        {/* {isKeyboardVisible && isSearching && ( */}
+          <FlatList
+            style={{
+              position: "absolute",
+              backgroundColor: "white",
+              width: "100%",
+            }}
+            data={services}
+            renderItem={({ item }) => {
+              return <SearchItem {...item} />;
+            }}
+          />
+        {/* )} */}
       </View>
     </View>
   );
@@ -27,6 +113,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 99,
     marginBottom: verticalScale(5),
     paddingHorizontal: horizontalScale(20),
     width: "100%",
