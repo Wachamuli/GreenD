364fc9da262581660ba6e1dbe6c8a28e47193547
Wachamuli <josemrr27@gmail.com>
2024-06-30 12:13:57 -0400

    Info and Error presets added to the useModal hook

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -5,14 +5,24 @@ import {
   useContext,
   useState,
 } from "react";
+import {
+  faCircleInfo,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+
 import MyModal, { ModalProps } from "../components/info/MyModal";
+import { ColorPalette } from "../styles/colorPalette";
 
 const ModalContext = createContext<{
   open: (p: ModalProps) => void;
   close: () => void;
+  info: (title: string, message: string) => void;
+  error: (message: string) => void;
 }>({
   open: () => {},
   close: () => {},
+  info: () => {},
+  error: () => {},
 });
 
 const ModalProvider = ({ children }: { children: ReactNode }) => {
@@ -29,8 +39,28 @@ const ModalProvider = ({ children }: { children: ReactNode }) => {
     setContent({});
   }, []);
 
+  const info = useCallback((title: string, message: string) => {
+    setVisible(true);
+    setContent({
+      title: title,
+      description: message,
+      iconProps: { icon: faCircleInfo, color: ColorPalette.secondary },
+      buttonOptions: [{ label: "Entendido" }],
+    });
+  }, []);
+
+  const error = useCallback((message: string) => {
+    setVisible(true);
+    setContent({
+      title: "¡Ups! Algo salió mal",
+      description: message,
+      iconProps: { icon: faTriangleExclamation, color: ColorPalette.error },
+      buttonOptions: [{ label: "Entendido" }],
+    });
+  }, []);
+
   return (
-    <ModalContext.Provider value={{ open, close }}>
+    <ModalContext.Provider value={{ open, close, info, error }}>
       {children}
       <MyModal {...content} isVisible={visible} onClose={close} />
     </ModalContext.Provider>
@@ -41,5 +71,5 @@ const useModal = () => {
   return useContext(ModalContext);
 };
 
-export { useModal }; 
+export { useModal };
 export default ModalProvider;
